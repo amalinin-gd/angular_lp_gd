2e1220956d8e8dacfe7f3189eee27e568762f101
amalinin <amalinin@griddynamics.com>
2018-05-17 13:39:43 +0400

    Few shared Angular and Sass components added.

--- src/storeApp/src/app/shared/shared.module.ts
+++ src/storeApp/src/app/shared/shared.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckboxComponent } from './components/checkbox/checkbox.component';
+import { RadioButtonGroupComponent } from './components/radio-button-group/radio-button-group.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    CheckboxComponent
+    CheckboxComponent,
+    RadioButtonGroupComponent,
+    SliderComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   exports: [
-    CheckboxComponent
+    CheckboxComponent,
+    RadioButtonGroupComponent,
+    SliderComponent,
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
