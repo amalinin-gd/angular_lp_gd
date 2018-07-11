08a986612d92b5eff08b224be8a5429c9a32115d
amalinin <amalinin@griddynamics.com>
2018-07-11 15:56:26 +0400

    Create a login form using Reactive Forms.

--- src/storeApp/src/app/pages/login/components/login-modal/login-modal.component.ts
+++ src/storeApp/src/app/pages/login/components/login-modal/login-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-login-modal',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-modal.component.scss']
 })
 export class LoginModalComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      login: new FormControl('', {validators: Validators.required, updateOn: 'blur'}),
+      password: new FormControl('', {validators: Validators.required, updateOn: 'blur'}),
+    });
   }
 
 }
--- src/storeApp/src/app/pages/login/login.module.ts
+++ src/storeApp/src/app/pages/login/login.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { LoginModalComponent } from './components/login-modal/login-modal.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {LoginModalComponent} from './components/login-modal/login-modal.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginPageComponent, LoginModalComponent],
   exports: [
     LoginPageComponent
   ]
 })
-export class LoginModule { }
+export class LoginModule {
+}
